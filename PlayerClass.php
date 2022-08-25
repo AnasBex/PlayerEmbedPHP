1a1b90c418a75041a7b9709045c2706185d4721f
anasbex647@gmail.com <anasbex647@gmail.com>
2022-08-25 16:16:45 +0700

    V1

--- PlayerClass.php
+++ PlayerClass.php
@@ -8,7 +8,7 @@ class bloggerStream {
     $timeout = 50;
     curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_USERAGENT,
-    "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0");
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
     $data = curl_exec($ch);
